167b1f39dc5eae3f37db3e1dc50e4b87ca268f8b
Ha Minh Phuong <ha.minhphuonghb@gmail.com>
2025-05-12 21:46:01 +0700

    learn component with state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import MyComponent from './components/MyComponent';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import React from 'react';
-class App extends React.Component {
+
+//#17
+class App17 extends React.Component {
   state ={
     name: "phuong",
     age: 23,
@@ -19,29 +21,30 @@ class App extends React.Component {
     );
   }
 }  
+export default App17;
 
 //#16 
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
+const App = () => {
+  const count = useSelector(state => state.counter.count);
+  const dispatch = useDispatch();
 
-//   return (
-//     <div>
-//       Hello World &amp; Ha Minh Phuong
-//       <MyComponent></MyComponent>
-//     </div>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Ha Minh Phuong
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
-// }
+  return (
+    <div>
+      Hello World &amp; Ha Minh Phuong
+      <MyComponent></MyComponent>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Ha Minh Phuong
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // </div>
+  );
+}
 
-export default App;
+// export default App16;
