8934699d91adaf1d941d7a3b94f3d4c4e83f03b2
Ha Minh Phuong <ha.minhphuonghb@gmail.com>
2025-05-15 14:36:37 +0700

    learn

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './ListMember.css';
 import MyComponent from './components/MyComponent';
-import InformationComponent from './components/InformationComponent';
+import ListMemberComponent from './components/ListMemberComponent';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import React from 'react';
+import InformationComponent from './components/InformationComponent';
 
 //#17
 class App17 extends React.Component {
   render(){ 
     return(
-      <InformationComponent/>
+      <ListMemberComponent/>
     )
   }
 }  
---
+++ src/components/ListMemberComponent.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+class ListMemberComponent extends React.Component {
+    // Data
+    state = {
+        name: "Ha Minh Phuong",
+        sex: "Male",
+        age: 23,
+        address: "Hoa Binh province"
+    }
+
+    // edit data
+    handleEditName = (event) => {
+        this.setState({
+            name: event.target.value,
+        })  
+        event.preventDefault();
+        console.log(this.state)
+    }
+    handleEditAge = (event) => {
+        this.setState({
+            name: event.target.value,
+        })  
+        event.preventDefault();
+        console.log(this.state)
+    }
+    handleEditSex = (event) => {
+        this.setState({
+            name: event.target.value,
+        })  
+        event.preventDefault();
+    }
+    handleEditAddress = (event) => {
+        this.setState({
+            name: event.target.value,
+        })  
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <div>   
+                <form onSubmit={(event) => {this.handleEditName(event)}}>
+                    <table id='head'>
+                        <tr>
+                            <td><label>Name:</label></td>
+                            <td><input type="text" id="fname" name="fname" value={this.state.name} onChange={(event) => this.handleEditName(event)}/><br/></td>
+                        </tr>
+                        <tr>
+                            <td><label>Age:</label></td>
+                            <td><input type="text" id="lname" name="lname" value={this.state.age} onChange={(event) => this.handleEditAge(event)}/><br/></td>
+                        </tr>
+                        <tr>
+                            <td><label>Sex:</label></td>
+                            <td><input type="text" id="lname" name="lname" value={this.state.sex} onChange={(event) => this.handleEditSex(event)}/><br/></td>
+                        </tr>
+                        <tr>
+                            <td><label>Address:</label></td>
+                            <td><input type="text" id="lname" name="lname" value={this.state.address} onChange={(event) => this.handleEditAddress(event)}/><br/></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><button type="submit">Submit</button></td>
+                        </tr>
+                    </table> 
+                </form>
+                <br/>
+                <hr/>
+                <br/>
+                <table id='content'>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th> 
+                        <th>Sex</th>
+                        <th>Address</th>
+                    </tr>
+                    <tr>
+                        <td>{this.state.name}</td>
+                        <td>{this.state.age}</td>
+                        <td>{this.state.sex}</td>
+                        <td>{this.state.address}</td>
+                    </tr>
+                    </table>
+            </div>
+        )
+    }
+}
+
+export default ListMemberComponent;
