f902b7954a1a19482cb3ae08ea6b6110ce176b2c
Ha Minh Phuong <ha.minhphuonghb@gmail.com>
2025-05-21 12:16:47 +0700

    learn Conditional output

--- src/components/DisplayInf.js
+++ src/components/DisplayInf.js
@@ -1,20 +1,48 @@
 import React from "react";
 
 class DisplayInf extends React.Component{
+    state = {
+        check: true   
+    }
+
+    // Click to hide infomation
+    handleClickHide = () => {
+        this.setState({
+            check: false
+        })
+    }
+
+    // Click to hide infomation
+    handleClickShow = () => {
+        this.setState({
+            check: true
+        })
+    }
+
     render(){
         const {listUsers} = this.props;
         return(
             <div>
-                <h1>Hiển thị thông tin</h1>
-                {listUsers.map((user) => {
-                    return(
-                        <div key={user.id}>
-                            <span>ID: {user.id}</span><br/>
-                            <span>Name: {user.name}</span><br/>
-                            <span>Age: {user.age}</span><hr/>
-                        </div>
-                    )
-                })}
+                <h1>Thông tin</h1>
+                {
+                    this.state.check?
+                    (<button onClick={() => this.handleClickHide()}>Ẩn thông tin</button>):
+                    <button onClick={() => this.handleClickShow()}>Hiện thông tin</button>
+                }
+                
+                {this.state.check && 
+                    <div>   
+                        {listUsers.map((user) => {
+                            return( 
+                                    <div key={user.id} className={+user.age > 18? "green":"red"}>
+                                        <span>ID: {user.id}</span><br/>
+                                        <span>Name: {user.name}</span><br/>
+                                        <span>Age: {user.age}</span><hr/>
+                                    </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,6 +8,10 @@ class MyComponent extends React.Component {
             {id:1, name:"John", age:25},
             {id:2, name:"Jane", age:30},
             {id:3, name:"Bob", age:35},
+            {id:4, name:"John Nathan", age:14},
+            {id:5, name:"Kelia", age:12},
+            {id:6, name:"Phuong", age:23},
+            {id:7, name:"David", age:11},
         ]
     }
 
